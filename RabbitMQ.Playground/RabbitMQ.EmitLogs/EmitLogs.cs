30ea5210ba44e671fc596822ae7d97b91204d78e
André Davis <CubicleJockey@users.noreply.github.com>
2022-01-05 09:13:33 -0800

    Publish/Subscribe Model

---
+++ RabbitMQ.Playground/RabbitMQ.EmitLogs/EmitLogs.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+using static System.Console;
+
+namespace RabbitMQ.EmitLogs
+{
+    internal class EmitLogs
+    {
+        public static void Main(string[] args)
+        {
+            const string EXCHANGE = "logs";
+
+            var factory = new ConnectionFactory { HostName = "localhost" };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: EXCHANGE, type: ExchangeType.Fanout);
+
+
+            var message = GetMessage(args);
+            var body = Encoding.UTF8.GetBytes(message);
+
+
+            channel.BasicPublish(exchange: EXCHANGE, routingKey: string.Empty, basicProperties: default, body: body);
+
+            WriteLine($" [x] Send {message}");
+
+            WriteLine(" Press ANY Key to exit.");
+            ReadLine();
+
+        }
+
+        private static string GetMessage(string[] args) => 
+            args.Length > 0 ? string.Join(" ", args) : "info: <args was empty>";
+    }
+}
---
+++ RabbitMQ.Playground/RabbitMQ.ReceiveLogs/ReceiveLog.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+using static System.Console;
+
+namespace RabbitMQ.ReceiveLogs
+{
+    internal class ReceiveLog
+    {
+        public static void Main()
+        {
+            const string EXCHANGE = "logs";
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+            var model = channel.QueueDeclare();
+            channel.QueueBind(queue: model.QueueName, exchange: EXCHANGE, routingKey: string.Empty);
+
+            Console.WriteLine(" [*] Waiting for logs.");
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                WriteLine($" [x] {message}");
+            };
+            
+            channel.BasicConsume(queue: model.QueueName,
+                autoAck: true,
+                consumer: consumer);
+
+            WriteLine(" Press [enter] to exit.");
+            ReadLine();
+        }
+    }
+}
